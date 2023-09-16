ed7b4d8f6f7ea2ada64d5e122c7372417812e586
iori2513 <yzhongtian194@gmail.com>
2023-09-16 22:25:01 +0900

    Initial Commit

---
+++ stoy-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stoy-ios
+//
+//  Created by 中田伊織 on 2023/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stoy-ios/stoy_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  stoy_iosApp.swift
+//  stoy-ios
+//
+//  Created by 中田伊織 on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct stoy_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
