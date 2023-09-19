a54c0a7167c35caa396523f9d65255233c805e26
iori2513 <yzhongtian194@gmail.com>
2023-09-19 20:56:56 +0900

    authentication画面作成

---
+++ stoy-ios/viewModels/AuthenticationViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  AuthenticationViewModel.swift
+//  stoy-ios
+//
+//  Created by 中田伊織 on 2023/09/19.
+//
+
+import Foundation
+import SwiftUI
+
+class AuthenticationViewModel: ObservableObject {
+    @Published var state: ViewState = ViewState.logIn
+    
+    func changeView() {
+        switch state {
+            case .logIn:
+                self.state = .signUp
+            case .signUp:
+                self.state = .logIn
+        }
+    }
+    
+    enum ViewState {
+        case logIn
+        case signUp
+        
+        var btnText: String {
+            switch self {
+                case .logIn:
+                    return "アカウント作成"
+                case .signUp:
+                    return "ログイン画面へ"
+            }
+        }
+    }
+}
--- stoy-ios/views/auth/AuthenticationView.swift
+++ stoy-ios/views/auth/AuthenticationView.swift
@@ -5,4 +5,27 @@
 //  Created by 中田伊織 on 2023/09/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct AuthenticationView: View {
+    
+    @StateObject private var viewModel = AuthenticationViewModel()
+    
+    var body: some View {
+        switch viewModel.state {
+            case .logIn:
+                LogInView()
+            case .signUp:
+                SignUpView()
+        }
+        Spacer().frame(height: 30)
+        Button(action: { viewModel.changeView() }, label: {
+            Text(viewModel.state.btnText)
+                .animation(.none)
+        })
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
