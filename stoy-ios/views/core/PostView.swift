8c56b696508af1f162b2de7132a4cdad89b3ab9e
iori2513 <yzhongtian194@gmail.com>
2023-10-16 14:51:46 +0900

    mainView作成

---
+++ stoy-ios/views/core/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  stoy-ios
+//
+//  Created by 中田伊織 on 2023/10/13.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
